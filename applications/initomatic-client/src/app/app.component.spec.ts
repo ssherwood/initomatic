2f9d7b464e06ed1f855c7c405298b323bdd00232
Shawn Sherwood <shawn.sherwood@gmail.com>
2018-06-03 18:14:43 -0400

    Fixed minor gradle annoyances with ngBuild in IntelliJ

--- applications/initomatic-client/src/app/app.component.spec.ts
+++ applications/initomatic-client/src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'initomatic'`, async(() => {
+  it(`should have as title 'Init-o-matic'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('initomatic');
+    expect(app.title).toEqual('Init-o-matic');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to initomatic!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Init-o-matic!');
   }));
 });
